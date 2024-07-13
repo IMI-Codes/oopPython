f0e04ebfdbaa902a0a0459321fac6c9c8f291a98
Manasseh <rougerapper@gmail.com>
2024-07-13 19:30:18 +0100

    learnt how to make my own module and started playing with inheritance in oop

---
+++ myModule/main.py
@@ -0,0 +1,10 @@
+#function definition
+
+def greet(name):
+  return f"Hey I'm {name}"
+
+class Athlete:
+  def __init__(self, name,age):
+    self.name = name
+    self.age = age
+  
---
+++ obj.py
@@ -0,0 +1,3 @@
+class criminal:
+  def __init__(self):
+    self.name
